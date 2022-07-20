2c0a60b6b8ff9f63ec017543ddf817319d1bc97c
Trần Kim Tín <tintk62@wru.vn>
2022-07-20 23:30:49 +0700

    a

--- server.js
+++ server.js
@@ -2,19 +2,49 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+app.get("/:name", function (req, res) {
+    let { name } = req.params;
+    let html = `
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Tín</title>
+            <style>
+                body {
+                    margin: 0;
+                    max-width: 100%;
+                    background-color: rgb(14, 14, 14);
+                    margin: 5%;
+                    /* padding-top: 10%; */
+                    /* padding-left: 35%; */
+                }
+            </style>
+        </head>
+        <body>
+            <video id="videoPlayer" width="90%" controls muted="muted" autoplay controlsList="nodownload">
+                <source src="/video/${name}" type="video/mp4"/>
+            </video>
+        </body>
+    </html>
+    `
+    res.send(html);
 });
 
-app.get("/video", function (req, res) {
+app.get("/video/:name", function (req, res) {
     try {
         const range = req.headers.range;
-        console.log(range)
+        const { name = '' } = req.params;
+        console.log({ name, range });
         if (!range) {
             res.status(400).send("Requires Range header");
         }
-        const videoPath = "video_test.mp4";
-        const videoSize = fs.statSync("video_test.mp4").size;
+        if (name == '') {
+            res.status(400).send("File error");
+        }
+        const videoPath = `./video/${name}.mp4`;
+        const videoSize = fs.statSync(videoPath).size;
         const CHUNK_SIZE = 10 ** 6;
         const start = Number(range.replace(/\D/g, ""));
         const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
@@ -31,7 +61,7 @@ app.get("/video", function (req, res) {
     }
     catch (err) {
         console.log(err);
-        res.send('error');
+        res.status(400).send("File error");
     }
 });
 
